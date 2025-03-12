1045033754dad40411d8a22d36e207bd7b9dcc00
BeataKostkowska <kostkowska.beata@gmail.com>
2025-03-12 12:44:06 +0100

    Show/hide details functionality

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -2,19 +2,28 @@ import { MdInfoOutline } from "react-icons/md";
 import { MdEditNote } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 import styles from "./Task.module.css";
+import { useState } from "react";
 
 function Task({ task }) {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
     <div className={styles.task_box}>
       <div className={styles.task_basic}>
         <p>{task.taskName}</p>
         <div className={styles.buttons_box}>
-          <MdInfoOutline className={`${styles.icon} ${styles.icon_info}`} />
+          <MdInfoOutline
+            className={`${styles.icon} ${styles.icon_info}`}
+            onClick={() => setShowDetails(!showDetails)}
+          />
           <MdEditNote className={`${styles.icon} ${styles.icon_edit}`} />
           <MdDeleteOutline className={`${styles.icon} ${styles.icon_delete}`} />
         </div>
       </div>
-      <div className={styles.task_details}>
+      <div
+        className={styles.task_details}
+        style={{ display: showDetails ? "block" : "none" }}
+      >
         <p>Priority: {task.priority}</p>
         <p>Deadline: {task.deadline} </p>
         <p>Created: {task.createdAt} </p>
@@ -28,7 +37,7 @@ function Task({ task }) {
           min="0"
           max="100"
           step="5"
-          value={task.progress}
+          // value={task.progress}
           className={styles.progress_input}
         />
       </div>
