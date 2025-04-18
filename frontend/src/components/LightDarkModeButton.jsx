2d52e78a15c3a60f678143e116c75202abd9eb96
BeataKostkowska <kostkowska.beata@gmail.com>
2025-04-18 20:13:44 +0200

    mode changes when browser settings are changed

--- frontend/src/components/LightDarkModeButton.jsx
+++ frontend/src/components/LightDarkModeButton.jsx
@@ -2,8 +2,6 @@ import { useEffect } from "react";
 import styles from "./LightDarkModeButton.module.css";
 
 import { MdDarkMode } from "react-icons/md";
-import { MdOutlineDarkMode } from "react-icons/md";
-import { MdLightMode } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
 import { useThemeStore } from "../store";
 
@@ -15,6 +13,38 @@ function LightDarkModeButton() {
     if (lightMode) document.body.setAttribute("data-theme", "light");
     if (!lightMode) document.body.setAttribute("data-theme", "dark");
   }, [lightMode]);
+
+  useEffect(() => {
+    const browserThemeSettings = window.matchMedia(
+      "(prefers-color-scheme: light)"
+    );
+
+    if (!localStorage.getItem("theme-storage")) {
+      localStorage.setItem(
+        "theme-storage",
+        `{"state":{"lightMode":${browserThemeSettings.matches}},"version":0}`
+      );
+    }
+
+    const handleBrowserSettingsChange = () => {
+      const modeInStorage = localStorage.getItem("theme-storage");
+      const currentMode = JSON.parse(modeInStorage).state.lightMode;
+
+      if (browserThemeSettings.matches !== currentMode) changeMode();
+    };
+
+    browserThemeSettings.addEventListener(
+      "change",
+      handleBrowserSettingsChange
+    );
+
+    return () => {
+      browserThemeSettings.removeEventListener(
+        "change",
+        handleBrowserSettingsChange
+      );
+    };
+  }, [changeMode]);
 
   return (
     <button className={styles.frame} onClick={changeMode}>
